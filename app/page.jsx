cbfaace07fdc1f0c2406cc6ba8a99270233b686f
Arthur Emmanuel <86601468+ArthurEmma2@users.noreply.github.com>
2023-05-16 07:15:40 +0000

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center" >Discover and share
+        <br  className="max-md:hidden"/>
+        <span className="orange_gradient text-center"> AI-powered Prompt</span></h1>
+        <p className="desc capitalize text-center">Discover best ai prompt for chatgpt, Bard and notion
+         lorem opensource best software with next js lets go </p>
+    </section>
+  );
 }
 
 export default Home;
