6c8eea547e037c748c3bca9b370b84b4eda4aadb
Arthur Emmanuel <86601468+ArthurEmma2@users.noreply.github.com>
2023-05-23 08:02:17 +0000

    update

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };
