901e599fe133fbeff5d414d8b243fc9101552c29
Arthur Emmanuel <86601468+ArthurEmma2@users.noreply.github.com>
2023-05-31 10:38:02 +0000

    update

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,8 +4,10 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "components/Form";
+import { RouteHandlerManager } from "next/dist/server/future/route-handler-managers/route-handler-manager";
 
 function CreatePrompt() {
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -19,8 +21,14 @@ function CreatePrompt() {
         method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
         }),
       });
+
+      if (response.ok) {
+        router.push("/");
+      }
     } catch (err) {
       console.log(err);
     }
