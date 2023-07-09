2986ca7677c6cb1808342417df2dabaf0a6af04a
Arthur Emmanuel <86601468+ArthurEmma2@users.noreply.github.com>
2023-07-09 14:04:47 +0000

    update

--- components/promptCard.jsx
+++ components/promptCard.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function PromptCard({ post }) {
   return (
     <div className="prompt-card">
-      <h3>{post.tag}</h3>
-      <h3>{post.prompt}</h3>
+  
     </div>
   );
 }
