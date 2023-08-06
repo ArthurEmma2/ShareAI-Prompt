ab3a04fc0e5edb7f84e8d8018a0bafda0fddae46
Arthur Emmanuel <86601468+ArthurEmma2@users.noreply.github.com>
2023-08-06 22:35:32 +0000

    Feat: changed the github auth to google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,67 +1,41 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 import { connectToDB } from "utils/database";
 import User from "models/user";
 
 const handler = NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
     async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        // Check if the user already exists based on the GitHub user ID
-        const existingUser = await User.findOne({
-          userId: profile.id.toString,
-        });
+        const userExists = await User.findOne({ email: profile.email });
 
-        if (existingUser) {
-          // User already exists, sign them in
-          return {
-            success: true,
-            message: "User found and signed in",
-            user: {
-              _id: existingUser._id.toString(),
-              userId: profile.id,
-              username: profile.name.replace("", "").toLowerCase(),
-              image: profile.avatar_url,
-            },
-          };
-        } else {
-          // User does not exist, create a new user
-          const newUser = await User.create({
-            userId: profile.id,
-            username: profile.name.replace("", "").toLowerCase(),
-            image: profile.avatar_url,
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
           });
-
-          // Sign in the newly created user
-          return {
-            success: true,
-            message: "New user created and signed in",
-            user: {
-              _id: newUser._id,
-              userId: profile.id,
-              username: profile.name.replace("", "").toLowerCase(),
-              image: profile.avatar_url,
-            },
-          };
         }
-      } catch (err) {
-        console.log(err);
-        return {
-          success: false,
-          message: "Error occurred while signing in",
-        };
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
       }
     },
   },
